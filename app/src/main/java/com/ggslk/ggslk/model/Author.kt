4c07e0ac345c498d8b5e7c38b1bd297a4b1c5e01
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-04-13 12:59:12 +0530

    Convert model classes to Kotlin

---
+++ app/src/main/java/com/ggslk/ggslk/model/Author.kt
@@ -0,0 +1,8 @@
+package com.ggslk.ggslk.model
+
+class Author {
+    var id: String? = null
+    var slug: String? = null
+    var name: String? = null
+    var profilePictureUrl: String? = null
+}
---
+++ app/src/main/java/com/ggslk/ggslk/model/Category.kt
@@ -0,0 +1,8 @@
+package com.ggslk.ggslk.model
+
+class Category {
+    var id: String? = null
+    var slug: String? = null
+    var title: String? = null
+    var featuredArticle: Article? = null
+}
