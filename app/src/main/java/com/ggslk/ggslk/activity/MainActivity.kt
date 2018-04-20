991f50f2063adf0acc702261449bac6c536f499c
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-04-20 14:11:22 +0530

    Close #12

--- app/src/main/java/com/ggslk/ggslk/activity/MainActivity.kt
+++ app/src/main/java/com/ggslk/ggslk/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ggslk.ggslk.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
@@ -9,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -20,13 +22,29 @@ import com.ggslk.ggslk.common.Session
 import com.ggslk.ggslk.fragment.*
 import com.ggslk.ggslk.model.Article
 import com.ggslk.ggslk.model.Category
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.onesignal.OneSignal
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
+private const val TAG = "GGSLKGoogleSignIn"
+private const val REQUEST_CODE_SIGN_IN = 1234
+private const val WEB_CLIENT_ID = "714868264744-j6jnk2rombdbs7ukd1jfaqqi3g8gce6t.apps.googleusercontent.com"
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onBackPressed() {
         val drawer = findViewById<View>(R.id.drawerLayout) as DrawerLayout
@@ -121,9 +139,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init()
 
-        // Call syncHashedEmail anywhere in your app if you have the user's email.
-        // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-        // OneSignal.syncHashedEmail(userEmail);
+        // Initialize Google sign in
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(WEB_CLIENT_ID)
+                .requestEmail()
+                .build()
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+
+        mAuth = FirebaseAuth.getInstance()
 
         // Initiate preferences
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
@@ -143,20 +170,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onStart() {
         super.onStart()
 
-        // Firebase Auth
-        val mAuth = FirebaseAuth.getInstance()
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth.currentUser
-        if (currentUser == null) {
-            mAuth.signInAnonymously().addOnCompleteListener(this) { task ->
-                if (!task.isSuccessful) {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(this@MainActivity, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+        signIn()
     }
 
     override fun onDestroy() {
@@ -166,6 +180,63 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         SaveHandler.save(this@MainActivity, "articles", Session.articles)
         SaveHandler.save(this@MainActivity, "favorites", Session.favorites)
         SaveHandler.save(this@MainActivity, "liked", Session.liked)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent();
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // successful -> authenticate with Firebase
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            } else {
+                // failed -> update UI
+                updateUI(null)
+                Toast.makeText(applicationContext, "SignIn: failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.e(TAG, "onConnectionFailed():$connectionResult");
+        Toast.makeText(applicationContext, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        var credential: AuthCredential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this@MainActivity, {
+                    if (it.isSuccessful) {
+                        // Sign in success
+                        Log.e(TAG, "signInWithCredential: Success!")
+                        var user: FirebaseUser = mAuth!!.currentUser!!
+                        updateUI(user)
+
+                        // Set OneSignal user email for better notifications
+                        OneSignal.setEmail(user.email!!)
+                    } else {
+                        // Sign in fails
+                        Log.w(TAG, "signInWithCredential: Failed!", it.exception)
+                        Toast.makeText(applicationContext, "Authentication failed!", Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                })
+    }
+
+    private fun signIn() {
+        val intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if(user != null){
+            Picasso.get().load(user.photoUrl).fit().centerCrop().into(navHeaderProfileImageView)
+            navHeaderNameTextView.text = user.displayName
+            navHeaderEmailTextView.text = user.email
+        }
     }
 
     private fun loadCachedSessionData() {
