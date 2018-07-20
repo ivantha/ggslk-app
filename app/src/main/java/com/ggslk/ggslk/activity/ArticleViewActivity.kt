340b74e749d1192bace03485e592e42b89ab0fd8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-07-20 13:07:58 +0530

    Fix Sinhala unicode issue
    Production release
    Closes #13
    Closes #15

--- app/src/main/java/com/ggslk/ggslk/activity/ArticleViewActivity.kt
+++ app/src/main/java/com/ggslk/ggslk/activity/ArticleViewActivity.kt
@@ -3,12 +3,15 @@ package com.ggslk.ggslk.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.Html
 import com.ggslk.ggslk.R
 import com.ggslk.ggslk.common.SaveHandler
 import com.ggslk.ggslk.common.Session
 import com.ggslk.ggslk.model.Article
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_article_view.*
+import android.webkit.WebViewClient
+import org.jsoup.Jsoup
 
 
 class ArticleViewActivity : AppCompatActivity() {
@@ -21,11 +24,21 @@ class ArticleViewActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        // Set article
+        // Enable JavaScript on WebView
+        articleActivityContentWebView.settings.javaScriptEnabled = true
+        articleActivityContentWebView.settings.javaScriptCanOpenWindowsAutomatically = true
+//        articleActivityContentWebView.setWebViewClient(WebViewClient())
+
+        // Format content to be mobile responsive
         var article: Article = intent.getSerializableExtra("article") as Article
-        articleActivityTitleTextView.text = article.title
+        val doc = Jsoup.parse(article.content)
+        doc.head().appendElement("link").attr("rel", "stylesheet").attr("type", "text/css").attr("href", "style.css");
+        article.content = doc.outerHtml()
+
+        // Set article
+        articleActivityTitleTextView.text = Html.fromHtml(article.title)
         Picasso.get().load(article.imageUrl).fit().centerCrop().into(articleActivityImageImageView)
-        articleActivityContentWebView.loadData(article.content, "text/html", "UTF-8")
+        articleActivityContentWebView.loadDataWithBaseURL("file:///android_asset/", article.content, "text/html", "UTF-8", null)
 
         // Set author
         articleActivityAuthorNameTextView.text = article.author!!.name
